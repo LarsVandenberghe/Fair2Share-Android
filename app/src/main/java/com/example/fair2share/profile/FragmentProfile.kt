4dee2bcb9f9db29e726a0b457a592f5dc8dd1ef9
LarsVandenberghe <lars.vandenberghe@student.hogent.be>
2019-11-17 21:33:16 +0100

    [fix] Refactoring

--- app/src/main/java/com/example/fair2share/profile/FragmentProfile.kt
+++ app/src/main/java/com/example/fair2share/profile/FragmentProfile.kt
@@ -9,26 +9,23 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import com.example.fair2share.R
-import com.example.fair2share.databinding.FragmentProfileFragmentBinding
-
+import com.example.fair2share.databinding.FragmentProfileBinding
 
 class FragmentProfile : Fragment() {
 
     private lateinit var viewModel: FragmentProfileViewModel
-    private lateinit var binding: FragmentProfileFragmentBinding
+    private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(FragmentProfileViewModel::class.java)
-        //binding.profileViewModel = viewModel.profile
         viewModel.profile.observe(this, Observer{ data ->
             binding.profile = data
         })
     }
-
 }
